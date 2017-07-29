413ccb1bf9c79835fe354ad40be86034f94184a0
Henpai Hsu <dreamfliper@gmail.com>
2017-07-30 00:44:54 +0800

    change to async and self-execute function

--- src/containers/app/resume.js
+++ src/containers/app/resume.js
@@ -8,19 +8,19 @@ import Dropbox from 'dropbox'
 class Resume extends Component {
 
     componentDidMount() {
-      let dbx = new Dropbox({accessToken:'UVoVCEKzMf4AAAAAAAAQQpNz6Ya0Bu0cAEqT_pHWX0iCyqgkmrsSiQeP1Dho6gQT'})
-			dbx.sharingGetSharedLinkFile({url: 'https://www.dropbox.com/s/zmkkxstuxje515o/Resume.md?dl=0'})
-				.then(function(data) {
-					console.log(data)
-					let blob = data.fileBlob
-					let filebuffer = new FileReader()
-					filebuffer.readAsText(blob)
-					filebuffer.onload = evt =>{
-						// console.log(evt.currentTarget.result)
-						store.dispatch(updateResume(evt.currentTarget.result))
-						store.dispatch(increment())
-					}
-			})
+      let dbx = new Dropbox({accessToken:'UVoVCEKzMf4AAAAAAAAQQpNz6Ya0Bu0cAEqT_pHWX0iCyqgkmrsSiQeP1Dho6gQT'});
+      (async function getResume(){
+				let data = await dbx.sharingGetSharedLinkFile({url: 'https://www.dropbox.com/s/zmkkxstuxje515o/Resume.md?dl=0'})
+				console.log(data)
+				let blob = data.fileBlob
+				let filebuffer = new FileReader()
+				filebuffer.readAsText(blob)
+				filebuffer.onload = evt =>{
+					// console.log(evt.currentTarget.result)
+					store.dispatch(updateResume(evt.currentTarget.result))
+					store.dispatch(increment())
+				}
+			})()
     }
 
     render() {
