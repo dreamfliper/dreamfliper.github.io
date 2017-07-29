76b43445cb69127b5acb442d4d076ab180ba59ae
Henpai Hsu <dreamfliper@gmail.com>
2017-07-30 03:10:26 +0800

    before eject

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import { Route, Link } from 'react-router-dom'
+import {Tab, Tabs} from 'react-toolbox/lib/tabs'
 import Home from './home'
 import About from './about'
 import Project from './projects'
 import Resume from './resume'
 
-const App = () => (
-  <div>
-    <header>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/projects">Project</Link>
-      <Link to="/resume">Resume</Link>
-    </header>
+class App extends Component {
+	state = {
+		index: 1,
+	}
 
-    <main>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/projects" component={Project} />
-      <Route exact path="/resume" component={Resume} />
-    </main>
-  </div>
-)
+	handleTabChange = (index) => {
+		this.setState({index});
+	}
+
+	handleActive = () => {
+		console.log('Special one activated');
+	}
+	
+	render() {
+		return (
+			<div>
+				<header>
+					<Link to="/">Home</Link>
+					<Link to="/about">About</Link>
+					<Link to="/projects">Project</Link>
+					<Link to="/resume">Resume</Link>
+				</header>
+
+				<Tabs index={this.state.index} onChange={this.handleTabChange}>
+					<Tab label='Primary'><small>Primary content</small></Tab>
+					<Tab label='Secondary' onActive={this.handleActive}><small>Secondary content</small></Tab>
+					<Tab label='Third' disabled><small>Disabled content</small></Tab>
+					<Tab label='Fourth' hidden><small>Fourth content hidden</small></Tab>
+					<Tab label='Fifth'><small>Fifth content</small></Tab>
+				</Tabs>
+
+				<main>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/about" component={About} />
+					<Route exact path="/projects" component={Project} />
+					<Route exact path="/resume" component={Resume} />
+				</main>
+			</div>
+		);
+	}
+}
 
 export default App
--- src/containers/app/resume.js
+++ src/containers/app/resume.js
@@ -1,43 +1,39 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { increment,updateResume } from '../../modules/counter'
+import { updateResume } from '../../modules/counter'
 import store from '../../store'
 import Markdown from 'react-remarkable'
 import Dropbox from 'dropbox'
 
 class Resume extends Component {
 
-    componentDidMount() {
-      let dbx = new Dropbox({accessToken:'UVoVCEKzMf4AAAAAAAAQQpNz6Ya0Bu0cAEqT_pHWX0iCyqgkmrsSiQeP1Dho6gQT'});
-      (async function getResume(){
-				let data = await dbx.sharingGetSharedLinkFile({url: 'https://www.dropbox.com/s/zmkkxstuxje515o/Resume.md?dl=0'})
-				console.log(data)
-				let blob = data.fileBlob
-				let filebuffer = new FileReader()
-				filebuffer.readAsText(blob)
-				filebuffer.onload = evt =>{
-					// console.log(evt.currentTarget.result)
-					store.dispatch(updateResume(evt.currentTarget.result))
-					store.dispatch(increment())
-				}
-			})()
-    }
+	componentDidMount() {
+		let dbx = new Dropbox({accessToken:'UVoVCEKzMf4AAAAAAAAQQpNz6Ya0Bu0cAEqT_pHWX0iCyqgkmrsSiQeP1Dho6gQT'});
+		(async ()=>{
+			let data = await dbx.sharingGetSharedLinkFile({url: 'https://www.dropbox.com/s/zmkkxstuxje515o/Resume.md?dl=0'})
+			console.log(data)
+			let blob = data.fileBlob
+			let filebuffer = new FileReader()
+			filebuffer.readAsText(blob)
+			filebuffer.onload = evt => store.dispatch(updateResume(evt.currentTarget.result))
+		})()
+	}
 
-    render() {
-        return (
-				  <div>
-				    <h1>Resume Page</h1>
-				    <Markdown source={this.props.resumeSource} />
-				  </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<h1>Resume Page</h1>
+				<Markdown source={this.props.resumeSource} />
+			</div>
+		)
+	}
 }
 
 
 const mapStateToProps = state => ({
-  resumeSource: state.counter.resumeSource,
+	resumeSource: state.counter.resumeSource,
 })
 
 export default connect(
-  mapStateToProps,
+	mapStateToProps,
 )(Resume)
