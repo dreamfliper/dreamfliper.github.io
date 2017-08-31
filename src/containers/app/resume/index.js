67c89bbd1c004d74969ed0a75a7e7d060af0b872
Henpai Hsu <dreamfliper@gmail.com>
2017-08-31 22:38:48 +0800

    add lang button on resume

--- src/containers/app/resume/index.js
+++ src/containers/app/resume/index.js
@@ -4,9 +4,31 @@ import { bindActionCreators } from 'redux'
 import { updateResume } from '../../../modules/counter'
 import Markdown from 'react-markdown'
 import Dropbox from 'dropbox'
-import { Row, Col } from 'antd'
+import { Row, Col, Button  } from 'antd'
 
 class Resume extends Component {
+	state = {
+		langSelect:0,
+		resumeLang:['English', 'Chinese']
+	}
+
+	handleClick = () =>{
+		this.setState({langSelect:+!this.state.langSelect})
+		let dbx = new Dropbox({accessToken:'UVoVCEKzMf4AAAAAAAAQQpNz6Ya0Bu0cAEqT_pHWX0iCyqgkmrsSiQeP1Dho6gQT'});
+		try{
+			(async ()=>{
+			const {fileBlob} = await dbx.filesDownload({
+				path: !!this.state.langSelect ? '/Resume.md':'/Resume_eng.md'
+			})
+			let filebuffer = new FileReader()
+			filebuffer.readAsText(fileBlob)
+			filebuffer.onload = evt => 
+				this.props.updateResume(evt.currentTarget.result)
+			})()
+		}	catch(err) {
+			console.log(err)
+		}
+	}
 
 	componentWillMount() {
 		let dbx = new Dropbox({accessToken:'UVoVCEKzMf4AAAAAAAAQQpNz6Ya0Bu0cAEqT_pHWX0iCyqgkmrsSiQeP1Dho6gQT'});
@@ -32,6 +54,9 @@ class Resume extends Component {
 			<Row>
 				<Col sm={{ span:21, offset:2 }} md={{ span:18, offset:3 }} lg={{ span:18, offset:4 }}>
 					<Markdown  source={this.props.resumeSource} />
+				</Col>
+				<Col>
+					<Button type="primary" onClick={()=>this.handleClick()} >{this.state.resumeLang[this.state.langSelect]}</Button>
 				</Col>
 			</Row>
 		)
