5fb1d96dae233456cd5a5bd841086330006016d3
Henpai Hsu <dreamfliper@gmail.com>
2017-09-11 17:26:13 +0800

    add english resume

--- src/containers/app/resume/index.js
+++ src/containers/app/resume/index.js
@@ -9,7 +9,7 @@ import { Row, Col, Button  } from 'antd'
 class Resume extends Component {
 	state = {
 		langSelect:0,
-		resumeLang:['English', 'Chinese']
+		resumeLang:['Chinese', 'English']
 	}
 
 	handleClick = () =>{
@@ -18,7 +18,7 @@ class Resume extends Component {
 		try{
 			(async ()=>{
 			const {fileBlob} = await dbx.filesDownload({
-				path: !!this.state.langSelect ? '/Resume.md':'/Resume_eng.md'
+				path: !!this.state.langSelect ? '/Resume_eng.md':'/Resume.md'
 			})
 			let filebuffer = new FileReader()
 			filebuffer.readAsText(fileBlob)
@@ -34,7 +34,7 @@ class Resume extends Component {
 		let dbx = new Dropbox({accessToken:'UVoVCEKzMf4AAAAAAAAQQpNz6Ya0Bu0cAEqT_pHWX0iCyqgkmrsSiQeP1Dho6gQT'});
 		try{
 			(async ()=>{
-			const {fileBlob} = await dbx.filesDownload({path: '/Resume.md'})
+			const {fileBlob} = await dbx.filesDownload({path: '/Resume_eng.md'})
 			let filebuffer = new FileReader()
 			filebuffer.readAsText(fileBlob)
 			filebuffer.onload = evt => 
@@ -52,7 +52,7 @@ class Resume extends Component {
 	render() {
 		return (
 			<Row>
-				<Col sm={{ span:21, offset:2 }} md={{ span:18, offset:3 }} lg={{ span:18, offset:4 }}>
+				<Col sm={{ span:21, offset:2 }} md={{ span:18, offset:3 }} lg={{ span:16, offset:4 }}>
 					<Markdown  source={this.props.resumeSource} />
 				</Col>
 				<Col>
