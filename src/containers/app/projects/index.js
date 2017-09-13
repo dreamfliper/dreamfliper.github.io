3cabc2b7209a67dfbdd066a1f643fd4746a31ac3
Henpai Hsu <dreamfliper@gmail.com>
2017-09-14 02:06:52 +0800

    add mobile demo preview

--- src/containers/app/projects/index.js
+++ src/containers/app/projects/index.js
@@ -19,49 +19,54 @@ const Wrapper = styled.section`
 	}
 `;
 
-const demovideo = (
+
+const demoflash = (
 		<img src="https://i.imgur.com/6yeANBf.gif" alt="demoflash" width="500px" />
 	)
-const demogif = (
+const demosub = (
 		<img src="https://i.imgur.com/U0xw99E.gif" alt="demosub"/>
 	)
 
 const Project = ({animationContainerReference}) => (
 <div ref={animationContainerReference} style={{width:'70vw'}}>
 	<Wrapper>
-			<Popover content={demovideo} title="Demo" >
-				<a className='sr-item--sequence' href="//github.com/dreamfliper/Flashlight/tree/master/PluginDirectories/1/merge.bundle">
-					<img src={flashlight} style={{ width: 150 }} alt='flashlight' />
-				</a>
-			</Popover>			<div className='sr-item-right'>
-				<b>
-					Merge Windows : A <a href="//flashlight.nateparrott.com">Flashlight</a> Plugin
-				</b>
-				<p>
-					Merge macOS application windows into tabs <br/>
-					example: make many finder windows into single window with tabs <br/>
-					<a href="https://www.dropbox.com/s/w7ildh25jcfag7x/merge_demo.mov?dl=0">demo</a> <br/>
-					合併 macOS 上多個視窗成為一個，變成好幾個分頁共用一個視窗 <br/>
-				</p>
-				<a className={`${styles.smSquareBtn} ${styles.githubaltBtn} `} href='//github.com/dreamfliper/Flashlight/tree/master/PluginDirectories/1/merge.bundle' > </a>
-				<Link className={`${styles.smrSquareBtn} ${styles.textfileBtn} `} to={`/note/Flashlight插件開發`} />
-			</div>
+		<Popover content={demoflash} title="Demo" >
+			<a className='sr-item--sequence' href="//github.com/dreamfliper/Flashlight/tree/master/PluginDirectories/1/merge.bundle">
+				<img src={flashlight} style={{ width: 150 }} alt='flashlight' />
+			</a>
+		</Popover>	
+		<div className='sr-item-right'>
+			<b>
+				Merge Windows : A <a href="//flashlight.nateparrott.com">Flashlight</a> Plugin
+			</b>
+			<p>
+				Merge macOS application windows into tabs <br/>
+				example: make many finder windows into single window with tabs <br/>
+				<a href="https://www.dropbox.com/s/w7ildh25jcfag7x/merge_demo.mov?dl=0">demo</a> <br/>
+				合併 macOS 上多個視窗成為一個，變成好幾個分頁共用一個視窗 <br/>
+			</p>
+			<a className={`${styles.smSquareBtn} ${styles.githubaltBtn} `} href='//github.com/dreamfliper/Flashlight/tree/master/PluginDirectories/1/merge.bundle' > </a>
+			<Link className={`${styles.smrSquareBtn} ${styles.textfileBtn} `} to={`/note/Flashlight插件開發`} />
+		</div>
+		<img src="https://i.imgur.com/6yeANBf.gif" alt="demoflash2" width="500px" />
 	</Wrapper>
 	
 	<Wrapper>
-			<div className='sr-item-left' >
-				<b>subTrans</b>
-				<p>A GUI tool swap subtitle between tchinese and schinese,<br/> powered by electron-vue and OpenCC</p>
-				<p>一個以 OpenCC 作為簡繁轉換的圖形介面工具</p>
-				<p>支援多種編碼，現代外觀，批次對文字檔簡繁轉換</p>
-				<a className={`${styles.smSquareBtn} ${styles.githubaltBtn} `} href='//github.com/dreamfliper/subTrans' > </a>
-				<Link className={`${styles.smrSquareBtn} ${styles.textfileBtn} `} to={`/note/subTrans開發與Vue入門`} />
-			</div>			
-	<Popover content={demogif} title="Demo" >
-		<a className='sr-item--sequence' href="//github.com/dreamfliper/subTrans">
-			<img src={subTrans} style={{ width: 150 }} alt='subTrans' />
-		</a>
-	</Popover>	</Wrapper>
+		<Popover content={demosub} title="Demo" >
+			<a className='sr-item--sequence' style={{order:4}} href="//github.com/dreamfliper/subTrans">
+				<img src={subTrans} style={{ width: 150 }} alt='subTrans' />
+			</a>
+		</Popover>	
+		<div className='sr-item-left' >
+			<b>subTrans</b>
+			<p>A GUI tool swap subtitle between tchinese and schinese,<br/> powered by electron-vue and OpenCC</p>
+			<p>一個以 OpenCC 作為簡繁轉換的圖形介面工具</p>
+			<p>支援多種編碼，現代外觀，批次對文字檔簡繁轉換</p>
+			<a className={`${styles.smSquareBtn} ${styles.githubaltBtn} `} href='//github.com/dreamfliper/subTrans' > </a>
+			<Link className={`${styles.smrSquareBtn} ${styles.textfileBtn} `} to={`/note/subTrans開發與Vue入門`} />
+		</div>	
+		<img src="https://i.imgur.com/U0xw99E.gif" alt="demosub2"/>
+	</Wrapper>
 
 	<Wrapper>
 		<a className='sr-item--sequence' href="//github.com/dreamfliper/subTrans">
