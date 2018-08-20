41af6e1aad5a29cf626700f05053b17fa33c0c2e
Henpai Hsu <dreamfliper@gmail.com>
2018-08-20 10:17:54 +0800

    add travisCi and styled-component flow type

--- src/containers/app/about/InheritProp.js
+++ src/containers/app/about/InheritProp.js
@@ -4,44 +4,44 @@ import { Component } from 'react'
 import RenderPropsTest from './RenderPropsTest'
 import styled from 'styled-components'
 
-export default class InheritProp extends Component<
-	{ passdown: string },
-	{ gotdata: string, value: string }
-> {
+type Props = { passdown: string }
+
+type State = { gotdata: string, value: string }
+
+type RenderProps = { message: string, name: string }
+
+export default class InheritProp extends Component<Props, State> {
 	state = {
-		gotdata: this.props.passdown,
+		// gotdata: this.props.passdown,
 		value: '',
 	}
 
 	// bad design, but I saw this everyday
-	componentWillReceiveProps(nextProps: any) {
-		if (nextProps !== this.props) {
-			this.setState({
-				gotdata: nextProps.passdown,
-			})
-		}
-	}
+	// componentWillReceiveProps(nextProps: Props) {
+	// 	if (nextProps !== this.props) {
+	// 		this.setState({
+	// 			gotdata: nextProps.passdown,
+	// 		})
+	// 	}
+	// }
 
 	handleChange = ({ target: { value: newValue } }: { target: { value: string } }) =>
 		this.setState({ value: newValue.replace(/\D/g, '') })
 
 	render() {
-		const reg: RegExp = /[-a-zA-Z0-9@:%._~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_.~#?&//=]*)/g
 		const str: string = '中文www.google.com也是沒問題中文www.googleeee.com也是沒問題'
 		const matched: Array<string> = str.match(reg) || []
 
 		return (
 			<div>
-				{this.state.gotdata}
-				<StyledProps gray='gray' />
+				{this./*state*/props.gotdata}
+				<StyledProps gray="gray" />
 				<input type="text" value={this.state.value} onChange={this.handleChange} />
 				{str
 					.split(reg)
-					.map(
-						phrase => (phrase === '' ? <a href={matched[0]}>{matched.shift()}</a> : phrase)
-					)}
+					.map(phrase => (phrase === '' ? <a href={matched[0]}>{matched.shift()}</a> : phrase))}
 				<RenderPropsTest name="renderprops">
-					{props => <p>{`${props.message} ${props.name}`}</p>}
+					{(props: RenderProps) => <p>{`${props.message} ${props.name}`}</p>}
 				</RenderPropsTest>
 			</div>
 		)
@@ -51,5 +51,6 @@ export default class InheritProp extends Component<
 const StyledProps = styled.div`
 	width: 30px;
 	height: 20px;
-	background-color: ${({gray}) => gray};
+	background-color: ${({ gray }: { gray: string }) => gray};
 `
+const reg: RegExp = /[-a-zA-Z0-9@:%._~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_.~#?&//=]*)/g
