6db8f2dab7c3b47e79cc6d5c95e6972fa68134b6
Henpai Hsu <dreamfliper@gmail.com>
2018-09-08 20:42:51 +0800

    adjust selector timeout for ci

--- tests/main.testcafe.js
+++ tests/main.testcafe.js
@@ -10,7 +10,7 @@ test('all integration test', async t => {
         .click(Selector('a').withText('Resume'))
         .expect(Selector('H1').withText('Henpai Hsu')).ok()
         .click(Selector('button').withText('Chinese'))
-        .click(Selector('button').withText('English'))
+        .click(Selector('button',{ timeout: 5000}).withText('English'))
         .click(Selector('a').withText('Notes'))
         .expect(Selector('ul').childNodeCount).gt(0, 'Dropbox return non-zero list')
         .click(Selector('a').withText('HomePage'))
